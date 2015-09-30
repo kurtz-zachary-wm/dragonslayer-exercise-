a642529c8b9c815a8da6c2d1ef0cc97277333030
Start Coding Student 2 <session2@START-Coding-MBP-C02PP8Q8G8WN.local>
2015-09-29 17:56:17 -0700

    Switching if statements to switch.

--- dragonslayer-exercise-.js
+++ dragonslayer-exercise-.js
@@ -3,6 +3,9 @@ var youHit = Math.floor(Math.random() * 2);
 var damageThisRound = Math.floor(Math.random() * 5 + 1);
 var totalDamage = 0;
 while(slaying) {
+    switch ("slaying") {
+        
+    }
     if (youHit) {
         console.log("You win!");
         totalDamage += damageThisRound;
